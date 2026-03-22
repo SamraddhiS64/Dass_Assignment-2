03bcc6e7ee69443f075ce06c5238d2c429f78e79
Samraddhi Singhai <singhaisamraddhi6@gmail.com>
2026-03-23 03:07:43 +0530

    data.py to store all the information for members, races, inventory, etc

---
+++ integration/code/data.py
@@ -0,0 +1,10 @@
+crew_members = {}   # key: name, value: {role, skill_level}
+inventory = {
+    "cars": [],
+    "spare_parts": 0,
+    "tools": 0,
+    "cash": 1000
+}
+races = []
+results = []
+missions = []
